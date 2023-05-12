cb258cea28fdacc9e3383dfcf3b23f7419544c0d
SouhailAloui <alouisouhail227@gmail.com>
2023-05-12 14:19:14 +0200

    done

--- quiz.js
+++ quiz.js
@@ -1,30 +1,30 @@
- // Questions will be asked
- const Questions = [{
+ // Questions to ask/////
+ var Questions = [{
 	id: 0,
-	q: "What is capital of India?",
-	a: [{ text: "gandhinagar", isCorrect: false },
+	q: "What is the capital of Ireland?",
+	a: [{ text: "Dublin", isCorrect: true },
 		{ text: "Surat", isCorrect: false },
-		{ text: "Delhi", isCorrect: true },
+		{ text: "Delhi", isCorrect: false },
 		{ text: "mumbai", isCorrect: false }
 	]
 
 },
 {
 	id: 1,
-	q: "What is the capital of Thailand?",
-	a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-		{ text: "phuket", isCorrect: false },
-		{ text: "Ayutthaya", isCorrect: false },
-		{ text: "Bangkok", isCorrect: true }
+	q: "How many stars are on the Chinese flag?",
+	a: [{ text: "3", isCorrect: false, isSelected: false },
+		{ text: "10", isCorrect: false },
+		{ text: "7", isCorrect: false },
+		{ text: "5", isCorrect: true }
 	]
 
 },
 {
 	id: 2,
-	q: "What is the capital of Gujarat",
+	q: "What is the capital of Canada?",
 	a: [{ text: "surat", isCorrect: false },
 		{ text: "vadodara", isCorrect: false },
-		{ text: "gandhinagar", isCorrect: true },
+		{ text: "Ottawa", isCorrect: true },
 		{ text: "rajkot", isCorrect: false }
 	]
 
@@ -32,37 +32,37 @@
 
 ]
 
-// Set start
+///// Set to start //
 var start = true;
 
-// Iterate
+////// Iterate /////
 function iterate(id) {
 
-// Getting the result display section
+// Get the result display section////
 var result = document.getElementsByClassName("result");
 result[0].innerText = "";
 
-// Getting the question
-const question = document.getElementById("question");
+////// Get the question///
+var question = document.getElementById("question");
 
 
-// Setting the question text
+/////// Setting the question text
 question.innerText = Questions[id].q;
 
-// Getting the options
-const op1 = document.getElementById('op1');
-const op2 = document.getElementById('op2');
-const op3 = document.getElementById('op3');
-const op4 = document.getElementById('op4');
+/////// Getting the options
+var op1 = document.getElementById('op1');
+var op2 = document.getElementById('op2');
+var op3 = document.getElementById('op3');
+var op4 = document.getElementById('op4');
 
 
-// Providing option text 
+//// Provid option text /////
 op1.innerText = Questions[id].a[0].text;
 op2.innerText = Questions[id].a[1].text;
 op3.innerText = Questions[id].a[2].text;
 op4.innerText = Questions[id].a[3].text;
 
-// Providing the true or false value to the options
+/// Provid the true or false value to the options /////
 op1.value = Questions[id].a[0].isCorrect;
 op2.value = Questions[id].a[1].isCorrect;
 op3.value = Questions[id].a[2].isCorrect;
@@ -70,7 +70,7 @@ op4.value = Questions[id].a[3].isCorrect;
 
 var selected = "";
 
-// Show selection for op1
+////// Show selection for op1 /////
 op1.addEventListener("click", () => {
 	op1.style.backgroundColor = "lightgoldenrodyellow";
 	op2.style.backgroundColor = "lightskyblue";
@@ -79,7 +79,7 @@ op1.addEventListener("click", () => {
 	selected = op1.value;
 })
 
-// Show selection for op2
+///// Show selection for op2 ////////
 op2.addEventListener("click", () => {
 	op1.style.backgroundColor = "lightskyblue";
 	op2.style.backgroundColor = "lightgoldenrodyellow";
@@ -88,7 +88,7 @@ op2.addEventListener("click", () => {
 	selected = op2.value;
 })
 
-// Show selection for op3
+//////// Show selection for op3//////
 op3.addEventListener("click", () => {
 	op1.style.backgroundColor = "lightskyblue";
 	op2.style.backgroundColor = "lightskyblue";
@@ -97,8 +97,8 @@ op3.addEventListener("click", () => {
 	selected = op3.value;
 })
 
-// Show selection for op4
-op4.addEventListener("click", () => {
+//////// Show selection for op4////////
+op4.addEventListener("click",() => {
 	op1.style.backgroundColor = "lightskyblue";
 	op2.style.backgroundColor = "lightskyblue";
 	op3.style.backgroundColor = "lightskyblue";
@@ -106,26 +106,28 @@ op4.addEventListener("click", () => {
 	selected = op4.value;
 })
 
-// Grabbing the evaluate button
-const evaluate = document.getElementsByClassName("evaluate");
+////////// Grabbing the evaluate button //////
+var evaluate = document.getElementsByClassName("evaluate");
 
-// Evaluate method
+// Evaluate method//////////
 evaluate[0].addEventListener("click", () => {
 	if (selected == "true") {
 		result[0].innerHTML = "True";
 		result[0].style.color = "green";
+		result[0].style.backgroundColor=" #000000db"
 	} else {
 		result[0].innerHTML = "False";
 		result[0].style.color = "red";
+		result[0].style.backgroundColor=" #000000db"
 	}
 })
 }
 
-if (start) {
+if(start) {
 iterate("0");
 }
 
-// Next button and method
+/////////// Next button and method////////////////
 const next = document.getElementsByClassName('next')[0];
 var id = 0;
 
